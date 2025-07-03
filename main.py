afa7c3b547f7b129438d97219ea5edd1eeecdecb
t4t45123 <tommypriest.tp@gmail.com>
2025-07-03 22:16:01 +0100

    changing vlc instance

--- main.py
+++ main.py
@@ -630,7 +630,7 @@ book = bookpaths[titleIndex]
 
 player = 0
 #player = vlc.MediaPlayer("/home/audiopi/e-Paper/RaspberryPi_JetsonNano/python/player/books/Arcanum Unbounded: Cosmere Collection.m4a")
-instance = vlc.Instance("--extraintf=dbus")
+instance = vlc.Instance("--extraintf=qt,dbus")
 media = 0
 if (settings != -1):
 	print("loadingbook" )
